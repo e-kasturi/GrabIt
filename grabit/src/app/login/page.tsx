847d3fd38ae474f3874ffa25a776cda8413f5a7e
indahkasturi <indahkasturiii@gmail.com>
2025-01-15 12:22:37 +0700

    test branch

---
+++ grabit/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+const Login =() =>{
+    return (
+        <div className="card bg-base-3flex w-3/4 max-w-5xl border border-gray-300 p-800 w-96 shadow-xl">
+        <span>hi</span>
+        </div>
+    )
+}
+export default Login
