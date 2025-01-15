37222bd18c1654dbdf4bda34f45969973673b1b8
nadiakasturi <nadiakasturi21@gmail.com>
2025-01-15 17:10:00 +0700

    produk

---
+++ grabit/src/db/models/produkModel.ts
@@ -0,0 +1,107 @@
+import { produkType } from "@/type";
+import { database } from "../config/config";
+import { z } from "zod";
+import { ObjectId } from "mongodb";
+
+const produkSchema = z.object({
+  name: z.string(),
+  slug: z.string(),
+  price: z.number(),
+  imgUrl: z.string(),
+  description: z.string(),
+  tags: z.array(z.string()),
+});
+
+class ProductModel {
+  static collection() {
+    return database.collection<produkType>("products");
+  }
+
+  static async create(newProduk: produkType) {
+    produkSchema.parse(newProduk);
+    newProduk.slug = newProduk.name.toLowerCase().replace(/\s+/g, "-");
+
+    return await this.collection().insertOne(newProduk);
+  }
+
+  static async findAll() {
+    return await this.collection().find().toArray();
+  }
+
+  static async findBySlug(slug: string) {
+    const product = await this.collection().findOne({
+      slug: slug,
+    });
+    return product;
+  }
+
+  static async deleteBySlug(slug: string) {
+    return await this.collection().deleteOne({ slug: slug });
+  }
+
+  static async updateBySlug(slug: string, newProduk: produkType) {
+    newProduk.slug = newProduk.name.toLowerCase().replace(/\s+/g, "-");
+    return await this.collection().updateOne(
+      { slug: slug },
+      {
+        $set: {
+          ...newProduk,
+          outletId: new ObjectId(newProduk.outletId),
+        },
+      }
+    );
+  }
+
+  static async findById(id: string) {
+    return await this.collection().findOne({ _id: new ObjectId(id) });
+  }
+
+  static async findAllwithOutletDetails(): Promise<produkType[]> {
+    const agg = [
+      {
+        $addFields: {
+          outletObjectId: { $toObjectId: "$outletId" },
+        },
+      },
+      {
+        $lookup: {
+          from: "outlets",
+          localField: "outletObjectId",
+          foreignField: "_id",
+          as: "outletDetails",
+        },
+      },
+      {
+        $unwind: {
+          path: "$outletDetails",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          name: 1,
+          slug: 1,
+          price: 1,
+          imgUrl: 1,
+          description: 1,
+          tags: 1,
+          "outletDetails.name": 1,
+          "outletDetails.nameOutlet": 1,
+          "outletDetails.phone": 1,
+          "outletDetails.email": 1,
+          "outletDetails.address": 1,
+        },
+      },
+    ];
+
+    return (await this.collection().aggregate(agg).toArray()) as produkType[];
+  }
+
+  static async findByOutletId(outletId: string) {
+    const objectId = new ObjectId(outletId);
+    console.log("objectId", objectId);
+    return await this.collection().find({ outletId: objectId }).toArray();
+  }
+}
+
+export default ProductModel;
--- grabit/src/type.ts
+++ grabit/src/type.ts
@@ -1,3 +1,5 @@
+import { ObjectId } from "mongodb";
+
 export type outletType = {
     name: string,
     email: string,
@@ -20,4 +22,42 @@ export type customerType = {
     longitude: number;
     imgUrl: string
     role: string
+}
+
+export type produkType = {
+ _id?: ObjectId
+ name: string
+ slug: string
+ price: number
+ imgUrl: string
+ description: string;
+ tags: string[]
+ thumbnail: string;
+ outletId?: ObjectId
+ outletDetails?: {
+    name: string
+    nameOutlet: string
+    phone: string
+    email: string
+    address: string
+ }
+}
+
+export type transactionType = {
+    _id?: string
+    outletId: string
+    customerId: string
+    transactionDate: string
+    totalAmount: number
+    status: string
+    customerDetail?: customerType[]
+}
+
+export type transaction = {
+    id: string
+    customerName: string
+    customerAddress: string
+    dateTransaction: string
+    status: string
+    updateStatus: string
 }
